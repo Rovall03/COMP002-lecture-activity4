6fd5ad8fc0665674452325a1565c16e12520691a
rosa ovalle <you@example.com>
2024-11-06 21:50:06 -0800

    added part 2

--- activity4.js
+++ activity4.js
@@ -2,3 +2,10 @@ let numbers = [1,2,3,4,5];
 
 console.log(numbers[0]);
 console.log(numbers[4]);
+
+//part 2
+for(let i=0; i<numbers.length; i++){
+    console.log(`numbers[${i}]: ${numbers[i]}`)
+}
+
+// part3
