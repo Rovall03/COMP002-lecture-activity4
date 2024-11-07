873a519c2748a00dd3f8b020dfa125dfb32f63c8
rosa ovalle <you@example.com>
2024-11-06 22:14:15 -0800

    pop added

--- activity4.js
+++ activity4.js
@@ -3,9 +3,22 @@ let numbers = [1,2,3,4,5];
 console.log(numbers[0]);
 console.log(numbers[4]);
 
+
 //part 2
 for(let i=0; i<numbers.length; i++){
-    console.log(`numbers[${i}]: ${numbers[i]}`)
+    console.log(`numbers[${i}]: ${numbers[i]}`);
 }
-
 // part3
+numbers.push(6,7,8,9,10);
+// new array is 1,2,3,4,5,6,7,8,9,10. 6-10 was added to the end
+console.log(numbers);
+
+
+//pop 3.2
+console.log(numbers.pop());
+console.log(numbers);
+console.log(numbers.pop());
+console.log(numbers);
+console.log(numbers.pop());
+console.log(numbers);
+////// contents after x3 pop is 1,2,3,4,5,6,7
